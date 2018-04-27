deb0ebb79806d33aebcdf4badae0536a7662a232
sina <sina.meraji@gmail.com>
2018-04-27 12:29:16 +0800

    chiayi's code

--- detect_obstacle.py
+++ detect_obstacle.py
@@ -1,25 +1,20 @@
-import cv2
-import numpy as np
 
-while(1):
-    img = cv2.imread('pinki.jpeg')
 
-    hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+def preprocess_image(frame):
+    import cv2
+    import numpy as np
+    frame = cv2.imread(frame)
 
-    
-    pink_min = np.uint8([[[233, 5, 150]]])
-    pink_max = np.uint8([[[339,73,100]]])
-    mask = cv2.inRange(hsv_img, cv2.cvtColor(pink_min,cv2.COLOR_BGR2HSV), cv2.cvtColor(pink_max,cv2.COLOR_BGR2HSV))
+    pink = np.uint8([[[147,20,255]]])
+    hsv_pink = cv2.cvtColor(pink,cv2.COLOR_BGR2HSV)
 
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(img,img, mask= mask)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    cv2.imshow('frame',img)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
+    lower_bound = np.array([hsv_pink[0][0][0]-20,147,147])
+    upper_bound = np.array([hsv_pink[0][0][0]+20,255,255])
 
-    # cv2.imwrite('output3.jpeg', res)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
-cv2.destroyAllWindows()
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
+
+    cv2.imwrite('res.jpeg', mask)
+
+preprocess_image('pink.jpeg')
